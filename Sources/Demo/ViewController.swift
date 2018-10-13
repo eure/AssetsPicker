a0c3cfba4c2f5786ea45b16389b61f316bd99cf7
Muukii <muukii.app@gmail.com>
2018-10-13 18:02:39 +0900

    Prepare Demo

--- Sources/Demo/ViewController.swift
+++ Sources/Demo/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+  @IBAction func didTapPresentButton(_ sender: Any) {
+    // Present AssetsPicker
+  }
 
 }
 
