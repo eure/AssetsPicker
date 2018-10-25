96ebd867c8a592b458a001c752bb5fb7257efb67
Aymen Rebouh <aymenmse@gmail.com>
2018-10-25 17:13:41 +0900

    Add a first version of a configurable picker

---
+++ Sources/AssetsPicker/Configuration/PhotoPicker.Configuration.swift
@@ -0,0 +1,62 @@
+//
+//  PhotoPicker.Configuration.swift
+//  AssetsPicker
+//
+//  Created by Aymen Rebouh on 2018/10/16.
+//  Copyright © 2018 eure. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension PhotosPicker {
+    public struct Configuration {
+        
+        internal static var shared = Configuration(selectionMode: .single, headerView: nil)
+        
+        public struct LocalizedStrings {
+            public var done: String = "Done"
+            public var next: String = "Next"
+            public var dismiss: String = "Dismiss"
+            public var collections: String = "Collections"
+        }
+        
+        public enum SelectionMode {
+            case single
+            case multiple(limit: Int)
+        }
+        
+        /// Single of multiple select
+        public let selectionMode: SelectionMode
+        
+        /// Color of asset selection
+        public let selectionColor: UIColor
+        
+        /// Color of asset selection
+        public let tintColor: UIColor
+        
+        /// Color of asset selection
+        public let numberOfItemsInRow: Int
+        
+        /// Localization of buttons
+        public var localize: LocalizedStrings = .init()
+        
+        /// Custom header view for assets collection
+        public let headerView: UIView?
+        
+        public init(
+            selectionMode: SelectionMode,
+            selectionColor: UIColor = .red,
+            tintColor: UIColor = .green,
+            numberOfItemsInRow: Int = 3,
+            headerView: UIView? = nil
+            ) {
+            self.selectionMode = selectionMode
+            self.selectionColor = selectionColor
+            self.tintColor = tintColor
+            self.numberOfItemsInRow = numberOfItemsInRow
+            self.headerView = headerView
+        }
+    }
+}
+
