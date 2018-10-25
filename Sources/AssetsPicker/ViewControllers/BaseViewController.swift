7458df5c9b695755cdabc77db9133c7c49533fab
Aymen Rebouh <aymenmse@gmail.com>
2018-10-25 17:13:02 +0900

    Add a base view controller that needs a view model

---
+++ Sources/AssetsPicker/ViewControllers/BaseViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BaseViewController.swift
+//  AssetsPicker
+//
+//  Created by Aymen Rebouh on 2018/10/16.
+//  Copyright © 2018 eure. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class BaseViewController<ViewModel>: UIViewController {
+    let viewModel: ViewModel
+    
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
