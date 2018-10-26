ca97f988152a6917ea93af1b694e8781764f9107
Aymen Rebouh <aymenmse@gmail.com>
2018-10-26 11:49:56 +0900

    Integrate picker to the demo project

--- Sources/Demo/ViewController.swift
+++ Sources/Demo/ViewController.swift
@@ -7,17 +7,32 @@
 //
 
 import UIKit
+import AssetsPicker
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  @IBAction func didTapPresentButton(_ sender: Any) {
-    // Present AssetsPicker
-  }
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func didTapPresentButton(_ sender: Any) {
+        let configuration = PhotosPicker.Configuration(
+            selectionMode: .multiple(limit: 3),
+            selectionColor: #colorLiteral(red: 0.4156862745, green: 0.768627451, blue: 0.8117647059, alpha: 1),
+            tintColor: #colorLiteral(red: 0.4156862745, green: 0.768627451, blue: 0.8117647059, alpha: 1),
+            numberOfItemsInRow: 3
+        )
+        let photoPicker = PhotosPicker.ViewController(withConfiguration: configuration)
+        photoPicker.pickerDelegate = self
+        present(photoPicker, animated: true, completion: nil)
+    }
 }
+
+extension ViewController: PhotosPickerDelegate {
+    func photoPicker(_ pickerController: PhotosPicker.ViewController, didPickImages images: [UIImage]) {
+        self.dismiss(animated: true, completion: nil)
+        print("main didPickImages = \(images)")
+    }
+}
+
 
